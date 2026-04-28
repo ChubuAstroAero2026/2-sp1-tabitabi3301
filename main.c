4aa8a63ce98861a397b1c5317522f4b309e75fbf
tabitabi3301 <au24004-5786@gs4e.chubu.ac.jp>
2026-04-28 12:29:01 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
-  //ここにコードを書く
+  double A; 
+    int B;   
+
+    scanf("%lf", &A);
+    scanf("%d", &B);
+
+    printf("%.*f\n", B, A);
 
   
   return 0;
